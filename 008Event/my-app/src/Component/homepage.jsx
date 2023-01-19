dd9aea246448c2c19fb1114c7764eded7751c75f
dor917 <donha917@naver.com>
2023-01-19 21:29:06 +0900

    찐막

---
+++ 008Event/my-app/src/Component/homepage.jsx
@@ -0,0 +1,12 @@
+function Hompage({name}) {
+    return (
+
+        <div>
+            <h1>{name} 나가라아아아ㅏ아아아</h1>
+            <button>나간다</button>
+        </div>
+    )
+
+}
+
+export default Hompage;
